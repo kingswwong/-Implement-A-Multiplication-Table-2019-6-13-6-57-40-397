23eb971e35a3ab3c9b8b846ce5e27b2bcf1bdaf8
“Dean” <467271883@qq.com>
2019-07-09 17:37:09 +0800

    add test case for number is invalid

--- __tests__/isValidNumber-spec.js
+++ __tests__/isValidNumber-spec.js
@@ -9,3 +9,24 @@ it('should return true when invoke start and end number when start is 2 and end 
     //then
     expect(result).toBe(true)
 })
+
+it('should return null when invoke start and end number when start is 5 and end is 4',() => {
+    //given
+    const start = 5;
+    const end = 4;
+    //when
+    const result = isValidNumber(5,4);
+    //then
+    expect(result).toBe(null)
+})
+
+
+it('should return null when invoke start and end number when start is -1 and end is 4',() => {
+    //given
+    const start = -1;
+    const end = 4;
+    //when
+    const result = isValidNumber(-1,4);
+    //then
+    expect(result).toBe(null)
+})
